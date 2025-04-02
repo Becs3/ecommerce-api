101d044b0faaa998bd7f7d1be4f80ac80d6927df
Rebecca Eklof <rebecca.ekloflofvenmark@medieinstitutet.se>
2025-04-02 13:52:05 +0200

    api

--- ecommerce-api-new/src/constants/env.ts
+++ ecommerce-api-new/src/constants/env.ts
@@ -1,5 +1,6 @@
 const getEnv = (key: string, defaultValue?: string) => {
   const value = process.env[key] || defaultValue;
+
   if (!value) {
     throw new Error(`Missing environment variable: ${key}`);
   }
@@ -12,5 +13,5 @@ export const DB_USER = getEnv('DB_USER');
 export const DB_PASSWORD = getEnv('DB_PASSWORD');
 export const DB_NAME = getEnv('DB_NAME');
 export const DB_PORT = parseInt(getEnv('DB_PORT', '3306'), 10);
-export const STRIPE_SECRET_KEY = getEnv('STRIPE_SECRET_KEY');
+export const STRIPE_SECRET_KEY = getEnv('SECRET_KEY');
 export const ACCESS_TOKEN_SECRET = getEnv('ACCESS_TOKEN_SECRET');
