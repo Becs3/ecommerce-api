57409ad2af9798d3d3e4aa4ee77a0b1c5a2f6f2a
Rebecca Eklof <rebecca.ekloflofvenmark@medieinstitutet.se>
2025-04-04 10:16:31 +0200

    change cors

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(cookieParser());
 app.use(cors({
   // origin: "http://localhost:5173",
-  origin: "*",
+  origin: "https://ecommerce-client-git-main-rebeccas-projects-424867e9.vercel.app",
   credentials: true,  // ✅ Allows cookies
 }));
 
