a396ef5ff650df610da98dd063e455476243121c
Rebecca Eklof <rebecca.ekloflofvenmark@medieinstitutet.se>
2025-04-07 10:35:29 +0200

    stripe

--- src/controllers/stripeController.ts
+++ src/controllers/stripeController.ts
@@ -4,6 +4,7 @@ import { STRIPE_SECRET_KEY } from "../constants/env";
 const stripe = require('stripe')(STRIPE_SECRET_KEY);
 
 export const checkoutSessionHosted = async (req: Request, res: Response) => {
+    
 };
 
 export const checkoutSessionEmbedded = async (req: Request, res: Response) => {
--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,7 @@ import productRouter from "./routes/products";
 import customerRouter from "./routes/customers";
 import orderRouter from "./routes/orders";
 import orderItemRouter from "./routes/orderItems";
-import stripeRouter from "./routes/stripe";
+//import stripeRouter from "./routes/stripe";
 import authRouter from "./routes/auth";
 import { updateOrder } from "./controllers/orderController";
 import { IProduct } from "./models/IProduct";
@@ -30,7 +30,7 @@ app.use('/products', productRouter)
 app.use('/customers', customerRouter)
 app.use('/orders', orderRouter)
 app.use('/order-items', orderItemRouter)
-app.use('/stripe', stripeRouter)
+//app.use('/stripe', stripeRouter)
 app.use('/auth', authRouter)
 
 // Attempt to connect to the database
