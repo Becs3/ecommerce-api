ad92b8f2d70850685e4e65266a3cb27a59c776ee
Rebecca Eklof <rebecca.ekloflofvenmark@medieinstitutet.se>
2025-04-07 11:32:10 +0200

    no slash

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(cookieParser());
 app.use(cors({
   // origin: "http://localhost:5173",
-  origin: "https://ecommerce-client-pi-three.vercel.app/",
+  origin: "https://ecommerce-client-pi-three.vercel.app",
   credentials: true,  // ✅ Allows cookies
 }));
 
